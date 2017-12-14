49220db42be4f58dce5082b5c2db1d82b0d1fca9
tjw5866 <tjw5866@rit.edu>
2017-12-13 21:49:12 -0500

    Fixed Particles
    VetexShader now known as VertexShader
    added particles to refraction, added anisotropic filtering

--- SmashingGallery/DX11Starter/Game.cpp
+++ SmashingGallery/DX11Starter/Game.cpp
@@ -147,7 +147,8 @@ void Game::Init()
 	samplerDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	samplerDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
-	samplerDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+	samplerDesc.Filter = D3D11_FILTER_ANISOTROPIC;
+	samplerDesc.MaxAnisotropy = 16;
 	samplerDesc.MaxLOD = D3D11_FLOAT32_MAX;
 	device->CreateSamplerState(&samplerDesc, &samplerState1);
 
@@ -172,8 +173,8 @@ void Game::Init()
 	device->CreateBlendState(&blend, &particleBlendState);
 
 	emitter = new Emitter(
-		1000,							// Max particles
-		100,							// Particles per second
+		100,							// Max particles
+		10,							// Particles per second
 		5,								// Particle lifetime
 		0.1f,							// Start size
 		5.0f,							// End size
@@ -591,6 +592,13 @@ void Game::RenderToTexture()
 	{
 		walls[i]->Draw(context);
 	}
+
+	float blend[4] = { 1,1,1,1 };
+	context->OMSetBlendState(particleBlendState, blend, 0xffffffff);
+	context->OMSetDepthStencilState(particleDepthState, 0);
+	emitter->Draw(context, camera);
+	context->OMSetBlendState(0, blend, 0xffffffff);
+	context->OMSetDepthStencilState(0, 0);
 
 	context->OMSetRenderTargets(1, &backBufferRTV, depthStencilView);
 
